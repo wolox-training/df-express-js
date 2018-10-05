bab077ffa9992849e1812062b1dc4a585ce6dcf4
dantefarias <fariasdante.94@gmail.com>
2018-10-05 11:47:43 -0300

    fixed singUp errors

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,6 +1,7 @@
 const logger = require('../logger'),
-  User = require('../models').user;
-const bcrypt = require('bcryptjs');
+  User = require('../models').user,
+  bcrypt = require('bcryptjs'),
+  errors = require('../errors');
 
 exports.create = (req, res, next) => {
   const emailDomain = '@wolox.com.ar';
@@ -15,18 +16,18 @@ exports.create = (req, res, next) => {
   logger.info(
     `Attempting to create user with name ${params.firstName} ${params.lastName} and email ${params.email}`
   );
-  User.findOne({ where: { email: params.email } }).then(resolve => {
+  User.findOne({ where: { email: params.email } }).then(userEmail => {
     const regex = new RegExp('^[0-9A-Za-z]+$');
-    if (resolve !== null) {
-      return res.status(400).send('The email already exist');
-    } else if (params.email && params.email.includes(emailDomain) === false) {
+    if (userEmail) {
+      return next(errors.uniqueEmailError);
+    } else if (!params.email.includes(emailDomain)) {
       // wrong domain
-      return res.status(400).send('Invalid email');
-    } else if ((params.contraseña && params.contraseña.length < 8) || !regex.test(params.contraseña)) {
+      return next(errors.invalidEmailError);
+    } else if (params.contraseña.length < 8 || !regex.test(params.contraseña)) {
       // short pass
-      return res.status(400).send('Invalid Password');
+      return next(errors.invalidPasswordError);
     } else {
-      // ahora vemos
+      // create user
       const saltRounds = 10;
       bcrypt.hash(params.contraseña, saltRounds).then(hash => {
         params.contraseña = hash;
